e7474e481558615140a90489504dbc97b8e6e3d0
jwa398 <jwa398@sfu.ca>
2022-07-26 14:49:36 -0700

    fixed pathing

--- redirects.php
+++ redirects.php
@@ -2,21 +2,21 @@
 if(isset($_POST['insert'])) {
   header("Location: http://localhost/insert/insert.html");
 } else if (isset($_POST['delete'])) {
-    header("Location: http://localhost/delete.html");
+    header("Location: http://localhost/delete/delete.html");
 } else if (isset($_POST['update'])) {
-    header("Location: http://localhost/update.html");
+    header("Location: http://localhost/update/update.html");
 } else if (isset($_POST['select'])) {
-    header("Location: http://localhost/select.html");
+    header("Location: http://localhost/select/select.html");
 } else if (isset($_POST['project'])) {
-    header("Location: http://localhost/project.html");
+    header("Location: http://localhost/project/project.html");
 } else if (isset($_POST['join'])) {
-    header("Location: http://localhost/join.html");
+    header("Location: http://localhost/join/join.html");
 } else if (isset($_POST['aggregate'])) {
-    header("Location: http://localhost/aggregate.html");
+    header("Location: http://localhost/aggregate/aggregate.html");
 } else if (isset($_POST['nested'])) {
-    header("Location: http://localhost/nest.html");
+    header("Location: http://localhost/nest/nest.html");
 } else if (isset($_POST['divide'])) {
-    header("Location: http://localhost/divide.html");
+    header("Location: http://localhost/divide/divide.html");
 }
   
 ?>
